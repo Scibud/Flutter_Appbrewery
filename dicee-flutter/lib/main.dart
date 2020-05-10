ec8cbc1b57437a2d1ee287a164594255b35f2500
Scibud <53430099+Scibud@users.noreply.github.com>
2020-05-11 00:56:33 +0300

    Random Dice
    Stateful widget workout

---
+++ dicee-flutter/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = 1, rightDiceNo = 6;
+
+  void dicepress() {
+    setState(() {
+      leftDiceNo = Random().nextInt(6) + 1;
+      rightDiceNo = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                dicepress();
+
+                print('Left button pressed $leftDiceNo');
+                print('Right Button  $rightDiceNo');
+              },
+              child: Image.asset('images/dice$leftDiceNo.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                dicepress();
+
+                print('Right Button pressed $rightDiceNo');
+                print('Left button  $leftDiceNo');
+              },
+              child: Image.asset('images/dice$rightDiceNo.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
